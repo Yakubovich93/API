86c3fcedf75142df6ad01aba83018b09bf990c3f
yakubovich.vi <yakuboshka93@mail.ru>
2021-04-15 12:19:12 +0300

    created API

---
+++ article/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+
+from article.views import ArticleView
+
+app_name = 'articles'
+
+urlpatterns = [
+    path('articles/', ArticleView.as_view()),
+    path('articles/<int:pk>', ArticleView.as_view()),
+]
